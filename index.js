7d5532eb3e6697f1c0aa9ac8ade6910a62b2db54
Carlos Andrade <carlos.andrade86@outlook.com>
2018-02-27 20:03:52 -0500

    Rob Rak Carlos Andrade---Travelers WIP

---
+++ index.js
@@ -0,0 +1,125 @@
+const container = document.getElementsByClassName("blocks");
+
+const blocks = container[0];
+const red = container[0].children[0];
+const blue = container[0].children[1];
+const green = container[0].children[2];
+const pink = container[0].children[3];
+const grey = container[0].children[4];
+
+blocks.style.width = "100%";
+let px = ;
+const timerInterval = window.setInterval(() => {
+},10);
+
+
+red.addEventListener("mousedown", (event) => {
+
+    red.style.transform ="translate(20px)";
+})
+
+
+
+
+// red.addEventListener("click", (event) => {
+    
+    //     red.style.order = "-1";
+    //     blue.style.order = "0";
+    //     green.style.order = "0";
+    //     pink.style.order = "0";
+    //     grey.style.order = "0";
+        
+    //   });
+      
+    //   blue.addEventListener("click", (event) => {
+        
+    //         blue.style.order = "-1";
+    //         red.style.order = "0";
+    //         green.style.order = "0";
+    //         pink.style.order = "0";
+    //         grey.style.order = "0";
+            
+    //   });
+      
+    //   green.addEventListener("click", (event) => {
+    //     blue.style.order = "0";
+    //     red.style.order = "0";
+    //     green.style.order = "-1";
+    //     pink.style.order = "0";
+    //     grey.style.order = "0";
+    //   });
+    
+    //   pink.addEventListener("click", (event) => {
+    //     blue.style.order = "0";
+    //     red.style.order = "0";
+    //     green.style.order = "0";
+    //     pink.style.order = "-1";
+    //     grey.style.order = "0";
+    //   });
+      
+    //   grey.addEventListener("click", (event) => {
+    //     blue.style.order = "0";
+    //     red.style.order = "0";
+    //     green.style.order = "0";
+    //     pink.style.order = "0";
+    //     grey.style.order = "-1";
+    //   });
+    
+
+// red.addEventListener("click", (event) => {
+    
+//     red.style.order = "-1";
+//     blue.style.order = "0";
+//     green.style.order = "0";
+//     pink.style.order = "0";
+//     grey.style.order = "0";
+    
+//   });
+  
+//   blue.addEventListener("click", (event) => {
+    
+//         blue.style.order = "-1";
+//         red.style.order = "0";
+//         green.style.order = "0";
+//         pink.style.order = "0";
+//         grey.style.order = "0";
+        
+//   });
+  
+//   green.addEventListener("click", (event) => {
+//     blue.style.order = "0";
+//     red.style.order = "0";
+//     green.style.order = "-1";
+//     pink.style.order = "0";
+//     grey.style.order = "0";
+//   });
+
+//   pink.addEventListener("click", (event) => {
+//     blue.style.order = "0";
+//     red.style.order = "0";
+//     green.style.order = "0";
+//     pink.style.order = "-1";
+//     grey.style.order = "0";
+//   });
+  
+//   grey.addEventListener("click", (event) => {
+//     blue.style.order = "0";
+//     red.style.order = "0";
+//     green.style.order = "0";
+//     pink.style.order = "0";
+//     grey.style.order = "-1";
+//   });
+
+ 
+
+
+// containerement's inline style attribute..addEventListener("click", (event) => {
+//     if(event.color === "black") {
+//         console.log(black);
+//     }
+// })
+
+// blue.addEventListener("click", (event) => {
+//     event.color = "black";
+// }) 
+
