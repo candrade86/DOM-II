21fe51e6a3fff3fdcba29874f99a9d960c335fcf
Robert Rak <robertrak@hotmail.com>
2018-02-28 22:28:50 -0500

    Added movement on mousedown

--- index.js
+++ index.js
@@ -5,121 +5,106 @@ const red = container[0].children[0];
 const blue = container[0].children[1];
 const green = container[0].children[2];
 const pink = container[0].children[3];
-const grey = container[0].children[4];
+const gray = container[0].children[4];
 
-blocks.style.width = "100%";
-let px = ;
-const timerInterval = window.setInterval(() => {
-},10);
+let redpos = 0;
+let bluepos = 0;
+let greenpos = 0;
+let pinkpos = 0;
+let graypos = 0;
+const origin = 0;
+const end = 480;
+
+//let redmousedown = false;
+
+blocks.style.width = '600px';
 
 
-red.addEventListener("mousedown", (event) => {
-
-    red.style.transform ="translate(20px)";
+red.addEventListener('mousedown', (event) => {
+  red.setAttribute('mousedown','true');
+})
+red.addEventListener("mouseup", (event) => {
+  red.setAttribute('mousedown','false');
+})
+red.addEventListener("mouseleave", (event) => {
+  red.setAttribute('mousedown','false');
+})
+blue.addEventListener('mousedown', (event) => {
+  blue.setAttribute('mousedown','true');
+})
+blue.addEventListener('mouseup', (event) => {
+  blue.setAttribute('mousedown','false');
+})
+blue.addEventListener('mouseleave', (event) => {
+  blue.setAttribute('mousedown','false');
+})
+green.addEventListener('mousedown', (event) => {
+  green.setAttribute('mousedown','true');
+})
+green.addEventListener('mouseup', (event) => {
+  green.setAttribute('mousedown','false');
+})
+green.addEventListener('mouseleave', (event) => {
+  green.setAttribute('mousedown','false');
+})
+pink.addEventListener('mousedown', (event) => {
+  pink.setAttribute('mousedown','true');
+})
+pink.addEventListener('mouseup', (event) => {
+  pink.setAttribute('mousedown','false');
+})
+pink.addEventListener('mouseleave', (event) => {
+  pink.setAttribute('mousedown','false');
+})
+gray.addEventListener('mousedown', (event) => {
+  gray.setAttribute('mousedown','true');
+})
+gray.addEventListener('mouseup', (event) => {
+  gray.setAttribute('mousedown','false');
+})
+gray.addEventListener('mouseleave', (event) => {
+  gray.setAttribute('mousedown','false');
 })
 
 
+const timerInterval = window.setInterval(() => {
+changePosition();
+moveBoxes();
+},10);
 
+function moveBoxes() {
+  red.style.transform = `translate(${redpos}px)`;
+  blue.style.transform = `translate(${bluepos}px)`;
+  green.style.transform = `translate(${greenpos}px)`;
+  pink.style.transform = `translate(${pinkpos}px)`;
+  gray.style.transform = `translate(${graypos}px)`;
+}
 
-// red.addEventListener("click", (event) => {
-    
-    //     red.style.order = "-1";
-    //     blue.style.order = "0";
-    //     green.style.order = "0";
-    //     pink.style.order = "0";
-    //     grey.style.order = "0";
-        
-    //   });
-      
-    //   blue.addEventListener("click", (event) => {
-        
-    //         blue.style.order = "-1";
-    //         red.style.order = "0";
-    //         green.style.order = "0";
-    //         pink.style.order = "0";
-    //         grey.style.order = "0";
-            
-    //   });
-      
-    //   green.addEventListener("click", (event) => {
-    //     blue.style.order = "0";
-    //     red.style.order = "0";
-    //     green.style.order = "-1";
-    //     pink.style.order = "0";
-    //     grey.style.order = "0";
-    //   });
-    
-    //   pink.addEventListener("click", (event) => {
-    //     blue.style.order = "0";
-    //     red.style.order = "0";
-    //     green.style.order = "0";
-    //     pink.style.order = "-1";
-    //     grey.style.order = "0";
-    //   });
-      
-    //   grey.addEventListener("click", (event) => {
-    //     blue.style.order = "0";
-    //     red.style.order = "0";
-    //     green.style.order = "0";
-    //     pink.style.order = "0";
-    //     grey.style.order = "-1";
-    //   });
-    
+function changePosition() {
+  if (red.getAttribute('mousedown') === 'true' && red.style.transform != `translate(${end}px)`) {
+    redpos += 1;
+  } else if (red.getAttribute('mousedown') === 'false' && red.style.transform != `translate(${origin}px)`) {
+    redpos -= 1;
+  }
+  if (blue.getAttribute('mousedown') === 'true' && blue.style.transform != `translate(${end}px)`) {
+    bluepos += 1;
+  } else if (blue.getAttribute('mousedown') === 'false' && blue.style.transform != `translate(${origin}px)`) {
+    bluepos -= 1;
+  }
+  if (green.getAttribute('mousedown') === 'true' && green.style.transform != `translate(${end}px)`) {
+    greenpos += 1;
+  } else if (green.getAttribute('mousedown') === 'false' && green.style.transform != `translate(${origin}px)`) {
+    greenpos -= 1;
+  }
+  if (pink.getAttribute('mousedown') === 'true' && pink.style.transform != `translate(${end}px)`) {
+    pinkpos += 1;
+  } else if (pink.getAttribute('mousedown') === 'false' && pink.style.transform != `translate(${origin}px)`) {
+    pinkpos -= 1;
+  }
+  if (gray.getAttribute('mousedown') === 'true' && gray.style.transform != `translate(${end}px)`) {
+    graypos += 1;
+  } else if (gray.getAttribute('mousedown') === 'false' && gray.style.transform != `translate(${origin}px)`) {
+    graypos -= 1;
+  }
 
-// red.addEventListener("click", (event) => {
-    
-//     red.style.order = "-1";
-//     blue.style.order = "0";
-//     green.style.order = "0";
-//     pink.style.order = "0";
-//     grey.style.order = "0";
-    
-//   });
-  
-//   blue.addEventListener("click", (event) => {
-    
-//         blue.style.order = "-1";
-//         red.style.order = "0";
-//         green.style.order = "0";
-//         pink.style.order = "0";
-//         grey.style.order = "0";
-        
-//   });
-  
-//   green.addEventListener("click", (event) => {
-//     blue.style.order = "0";
-//     red.style.order = "0";
-//     green.style.order = "-1";
-//     pink.style.order = "0";
-//     grey.style.order = "0";
-//   });
-
-//   pink.addEventListener("click", (event) => {
-//     blue.style.order = "0";
-//     red.style.order = "0";
-//     green.style.order = "0";
-//     pink.style.order = "-1";
-//     grey.style.order = "0";
-//   });
-  
-//   grey.addEventListener("click", (event) => {
-//     blue.style.order = "0";
-//     red.style.order = "0";
-//     green.style.order = "0";
-//     pink.style.order = "0";
-//     grey.style.order = "-1";
-//   });
-
- 
-
-
-// containerement's inline style attribute..addEventListener("click", (event) => {
-//     if(event.color === "black") {
-//         console.log(black);
-//     }
-// })
-
-// blue.addEventListener("click", (event) => {
-//     event.color = "black";
-// }) 
-
+}
